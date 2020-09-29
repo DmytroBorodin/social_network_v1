8ab7277666cdad91ba50016b1fbb6cc1e39c45ea
Dmytro Borodin <dimabor96@gmail.com>
2020-09-29 22:59:56 +0300

    Updates

---
+++ precache-manifest.0f50a8c685c834c964f4625947119e20.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed76a4a77bccdca281a8e5cd98c070b6",
+    "url": "/dmytroborodin.github.io/social_network_v1/index.html"
+  },
+  {
+    "revision": "6dbceb527e7497eb4587",
+    "url": "/dmytroborodin.github.io/social_network_v1/static/css/main.5c00888f.chunk.css"
+  },
+  {
+    "revision": "a8353e28f454a705d3f3",
+    "url": "/dmytroborodin.github.io/social_network_v1/static/js/2.f3244739.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dmytroborodin.github.io/social_network_v1/static/js/2.f3244739.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dbceb527e7497eb4587",
+    "url": "/dmytroborodin.github.io/social_network_v1/static/js/main.c2b11005.chunk.js"
+  },
+  {
+    "revision": "21490c19bddf1bb07df6",
+    "url": "/dmytroborodin.github.io/social_network_v1/static/js/runtime-main.e3312e32.js"
+  },
+  {
+    "revision": "9d3b8c4d39e1a7bc531cfb8b12b90d68",
+    "url": "/dmytroborodin.github.io/social_network_v1/static/media/loupe.9d3b8c4d.png"
+  },
+  {
+    "revision": "6eb99065a5d53cc168f6c6a4c9497f95",
+    "url": "/dmytroborodin.github.io/social_network_v1/static/media/user.6eb99065.jpg"
+  }
+]);
